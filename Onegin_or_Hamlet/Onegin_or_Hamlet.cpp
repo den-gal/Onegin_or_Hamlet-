6606604426cd2034d40702f975cd9cf0c285ee43
den-gal <62265042+den-gal@users.noreply.github.com>
2020-10-07 17:48:09 +0300

    Begining
    Transport from windows to linux

---
+++ Onegin_or_Hamlet/Onegin_or_Hamlet.cpp
@@ -0,0 +1,38 @@
+﻿// Denis Galanin (327)
+// Onegin_or_Hamlet task for special course of SP CMC  
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+//#include <sys/mman.h>
+void Sort_the_Poem();
+char* Input_Text(FILE* fd);
+
+
+char* Input_Text(FILE * fd)
+{
+    if (fd == NULL)
+    {
+        fprintf(stderr, "ERROR: FILE %s wasn't opened sucssessfuly", poem);
+        _exit(1);
+    }
+
+}
+
+void Sort_the_Poem()
+{
+    const char* poem;
+    gets(poem);
+    FILE* fd = fopen(poem, "r+");
+
+    const char* poem;
+    fclose(fd);
+}
+
+int main()
+{
+    printf("Start of execution");
+    Sort_the_Poem();
+    
+    printf("End of execution");
+}
+
