571ef30f9f330aad7ffab52c18ec95a9ae9c961e
den-gal <Galanin-cbyltrc@ya.ru>
2020-10-08 00:58:26 +0300

    fixes for mmap from linux

--- Onegin_or_Hamlet/Onegin_or_Hamlet.cpp
+++ Onegin_or_Hamlet/Onegin_or_Hamlet.cpp
@@ -1,48 +1,52 @@
 ﻿// Denis Galanin (327)
 // Onegin_or_Hamlet task for special course of SP CMC  
+
 #include <iostream>
-#include <cstdio>
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <stdlib.h>
-void Sort_the_Poem();
-char* Input_Text(FILE* fd);
+#include <fcntl.h>
+#include <sys/un.h>
+#include <sys/io.h>
+#include <unistd.h>
+#include <assert.h>
 
+void Sort_the_Poem();
+char* Input_Text(int* fd);
 
 char* Input_Text(int *fd)
 {
     char* novel = NULL;
-    if (fd == NULL)
+    if (*fd == -1)
     {
         fprintf(stderr, "ERROR: FILE wasn't opened sucssessfuly");
-        _exit(1);
+        exit(1);
     }
     struct stat statistics;
     if(fstat( *fd, &statistics) != -1)
     {
         int len = statistics.st_size;
-        //novel = mmap(0, len, PROT_READ | PROT_WRITE, MAP_PRIVATE,fd);
+        novel = (char*)mmap(0, len, PROT_READ | PROT_WRITE, MAP_PRIVATE,*fd,0);
     }
     else
     {
         fprintf(stderr, "ERROR: FILE len is incorrect");
-        _exit(1);
+        exit(1);
     }
     return novel;
 }
 
 void Sort_the_Poem()
 {
-    const char* poem;
-    int fd = open(poem, "r+");
+    char* poem;
+    fgets(poem, 1000, stdin);
+    int fd = open(poem, O_RDWR);
+    char * novel = Input_Text(&fd);
 
-    //while (!feof(fd))
-    //{
-    //   fgets(str, 1000, fd);
-    //}
+
     close(fd);
 }
 
